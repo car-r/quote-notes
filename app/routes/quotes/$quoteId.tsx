793de988aa46cd5e73668d13b2bce443f4194f23
Carson Richter <carson.ri@protonmail.com>
2022-05-29 20:39:46 -0700

    delete button on quote route

--- app/routes/quotes/$quoteId.tsx
+++ app/routes/quotes/$quoteId.tsx
@@ -1,4 +1,5 @@
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
+import { redirect } from "@remix-run/server-runtime";
 import { prisma } from "~/db.server";
 
 export const loader = async ({params}: any) => {
@@ -11,6 +12,14 @@ export const loader = async ({params}: any) => {
     return {author, quote}
 }
 
+export const action =async ({ request, params }: any) => {
+    const form = await request.formData()
+    if(form.get('_method') === 'delete') {
+        await prisma.quote.delete({ where: { id: params.quoteId}})
+        return redirect('/quotes')
+    }
+}
+
 export default function QuoteDetail() {
     const quote = useLoaderData()
     console.log(quote)
@@ -18,6 +27,15 @@ export default function QuoteDetail() {
         <div>
             <h2>Quote Detail</h2>
             <p>{quote.quote.body}</p>
+            <Form method="post">
+                <button
+                    name="_method"
+                    value="delete"
+                    className="px-4 py-2 bg-green-500"
+                >
+                    Delete
+                </button>
+            </Form>
         </div>
     )
 }
