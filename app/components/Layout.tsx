2a47e6b01c842d115e21afb44b6414ecc83985aa
Carson Richter <carson.ri@protonmail.com>
2022-06-19 15:48:37 -0700

    first sidebar implementation with routes

--- app/components/Layout.tsx
+++ app/components/Layout.tsx
@@ -1,6 +1,9 @@
+import Sidebar from "./Sidebar";
+
 export default function Layout({children}: any) {
     return (
-        <div className="flex flex-col px-4 mx-auto text-stone-300/60">
+        <div className="flex px-4 mx-auto text-stone-300/60">
+            <Sidebar />
             {children}
         </div>
     )
--- app/routes/quotes/index.tsx
+++ app/routes/quotes/index.tsx
@@ -17,15 +17,17 @@ export default function QuotesIndex() {
     console.log(data)
     return (
         <>
-            <div>Quotes Index Page</div>
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                {data.quotes.map((quote: any) => (
-                    <Link to={`/quotes/${quote.id}`} key={quote.id}
-                        className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
-                    >
-                        {quote.body}
-                    </Link>))
-                }
+            <div className="flex flex-col pt-10">
+                <h3 className="text-xl">Quotes</h3>
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+                    {data.quotes.map((quote: any) => (
+                        <Link to={`/quotes/${quote.id}`} key={quote.id}
+                            className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
+                        >
+                            {quote.body}
+                        </Link>))
+                    }
+                </div>
             </div>
             
            
