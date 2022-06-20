18fc42eba3851da1b84d358f5b795972e5ba7baa
Carson Richter <carson.ri@protonmail.com>
2022-06-19 18:29:58 -0700

    added author cards

--- app/routes/content/index.tsx
+++ app/routes/content/index.tsx
@@ -10,19 +10,26 @@ export default function ContentIndex() {
     const data = useLoaderData()
     console.log(data)
     return (
-        <div>
-            <h2>Your Content</h2>
+        <div className="flex flex-col pt-10">
+            <h3 className="text-xl pb-6">Saved Content</h3>
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                 {data.map((content: any) => (
                     <Link to={`/content/${content.id}`} key={content.id}
                     className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
                     >
-                    <div>
+                    <div className="pb-2">
                         <img src={content.imgUrl} alt={content.title} 
                             className="object-fit max-w-96"
                         />
                     </div>
-                    {content.title}
+                    <div>
+                        <p className="font-bold">
+                            {content.title}
+                        </p>     
+                        <p className="text-sm font-thin tracking-wider">
+                            {content.authorName}
+                        </p>               
+                    </div>
                     </Link>
                 ))}
             </div>
