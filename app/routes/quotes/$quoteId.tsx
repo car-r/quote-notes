5259609f4bde51db5a54dd4d9090a209756c57de
Carson Richter <carson.ri@protonmail.com>
2022-06-21 17:27:14 -0700

    implemented first quoteNote and displayed to quoteId route

--- app/routes/quotes/$quoteId.tsx
+++ app/routes/quotes/$quoteId.tsx
@@ -13,21 +13,32 @@ export const loader = async ({params}: any) => {
     const content = await prisma.content.findUnique({
         where: { id: quote?.contentId}
     })
-    return {author, quote, content}
+
+    const notes = await prisma.quoteNote.findMany({
+        where: {quoteId: params.quoteId}
+    })
+    return {author, quote, content, notes}
 }
 
 export const action =async ({ request, params }: any) => {
     const form = await request.formData()
+    const formBody = form.get('body')
+    console.log(Object.fromEntries(form))
+    const userId = 'cl4kuy4wu0009lnmfgbvhhww8'
+
     if(form.get('_method') === 'delete') {
         await prisma.quote.delete({ where: { id: params.quoteId}})
         return redirect('/quotes')
     }
 
     // add note Action
-    // if(form.get('_method') !== 'delete') {
-    //     await prisma.quoteNote.create({ where: {quoteId: params.quoteId}})
-    //     return redirect(`/quotes/${params.quoteId}`)
-    // }
+    if(form.get('_method') !== 'delete') {
+        const body = formBody
+        const quoteId = params.quoteId
+        const fields = {body, quoteId, userId}
+        await prisma.quoteNote.create({ data: fields })
+        return redirect(`/quotes/${params.quoteId}`)
+    }
 }
 
 export default function QuoteDetail() {
@@ -63,6 +74,11 @@ export default function QuoteDetail() {
                         <button className="px-4 py-2 bg-blue-400 rounded text-white hover:bg-blue-600">Add Note</button>
                     </Form>
                 </div>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                {quote.notes.map((note: any) => (
+                    <div>{note.body}</div>
+                ))}
             </div>
         </div>
     )
