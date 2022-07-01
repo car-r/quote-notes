5f110f1ee92f979847ae86e2a4ef67537e4b13a1
Carson Richter <carson.ri@protonmail.com>
2022-07-01 13:56:06 -0700

    isFavorited update to quote model in schema + first implementation using input value instead of checkbox

--- app/routes/quotes/index.tsx
+++ app/routes/quotes/index.tsx
@@ -1,5 +1,6 @@
-import { Link, useLoaderData } from "@remix-run/react";
-import { json } from "@remix-run/server-runtime";
+import { Form, Link, useLoaderData } from "@remix-run/react";
+import { json, redirect } from "@remix-run/server-runtime";
+import { useState } from "react";
 import AddQuoteCard from "~/components/AddQuoteCard";
 
 import { prisma } from "~/db.server";
@@ -16,12 +17,24 @@ export const loader = async () => {
     return {quotes, authors}
 }
 
+export const action = async ({request}: any) => {
+    const form = await request.formData()
+    const id = form.get('id')
+    const stringIsFavorited = form.get('isFavorited')
+    const isFavorited = JSON.parse(stringIsFavorited)
+    console.log(id + isFavorited)
+
+    const quote = await prisma.quote.update({
+        where: { id: id },
+        data: { isFavorited: isFavorited }
+    })
+    return redirect('/quotes')
+}
+
 export default function QuotesIndex() {
 
     const data = useLoaderData()
-    // const quotes = data[0]
-    // const authors = data[1]
-    console.log(data)
+
     return (
         <>
             <div className="flex flex-col pt-10 max-w-4xl">
@@ -33,12 +46,38 @@ export default function QuotesIndex() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <AddQuoteCard />
                     {data.quotes.map((quote: any) => (
-                        <Link to={`/quotes/${quote.id}`} key={quote.id}
-                            className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
-                        >
-                            <p className="text-xl text-center pb-6 italic font-semibold">"{quote.body}"</p>
-                            <p className="font-light"><Link to={`/authors/${quote.authorId}`}>{quote.authorName}</Link></p>
-                        </Link>))
+                        <div key={quote.id}
+                            className="flex flex-col p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
+                        >   
+                            <div className="flex-1">
+                                <Form method="post">
+                                    <div onClick={() => console.log('clicked')} className="flex justify-end mb-1">   
+                                        <div className="flex flex-col">
+                                        <input type="hidden" name="id" value={quote.id}/>
+                                        <input type="hidden" name="isFavorited" value={!quote.isFavorited}/>
+                                        {/* <input type="checkbox" name="isFavorited" onChange={() => toggleCheckbox(quote.isFavorited)} checked={favoriteState}/> */}
+                                            <button type="submit" className="">
+                                                {quote.isFavorited ? 
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
+                                                        <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+                                                    </svg>
+                                                    :
+                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-right" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                                                    </svg>
+                                                }
+                                            </button>
+                                        </div>
+                                    </div>
+                                </Form>
+                                <Link to={`/quotes/${quote.id}`}>
+                                    <div className="flex flex-col h-5/6 ">
+                                        <p className="text-xl text-center pb-6 italic font-semibold">"{quote.body}"</p>
+                                        <p className="font-light mt-auto"><Link to={`/authors/${quote.authorId}`}>{quote.authorName}</Link></p>
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>))
                     }
                 </div>
             </div>
