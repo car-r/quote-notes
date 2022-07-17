12617e864dc8e5be3416f45aee1d2d4643d4c16d
Carson Richter <carson.ri@protonmail.com>
2022-07-16 17:57:52 -0700

    quoteNoteQuoteCard component moved to components folder

--- app/routes/quoteNotes/$quoteNoteId.tsx
+++ app/routes/quoteNotes/$quoteNoteId.tsx
@@ -2,6 +2,7 @@ import { Form, Link, useLoaderData } from "@remix-run/react"
 import { redirect } from "@remix-run/server-runtime"
 import QuoteNote from "~/components/QuoteNote"
 import QuoteNoteDeleteCard from "~/components/QuoteNoteDeleteCard"
+import QuoteNoteQuoteCard from "~/components/QuoteNoteQuoteCard"
 import { prisma } from "~/db.server"
 
 export const loader = async ({params}: any) => {
@@ -42,37 +43,14 @@ export default function QuoteNoteId() {
                     <QuoteNote note={data.data}/>
                 </div>
                 <div className="flex flex-col gap-4 col-end-4 col-span-1">
-                    <Link to={`/quotes/${data.quote.id}`} >
+                    <QuoteNoteQuoteCard quote={data}/>
+                    {/* <Link to={`/quotes/${data.quote.id}`} >
                         <div className="p-4  border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400">
                             <p className="text-xl text-center pb-6 italic font-semibold">"{data.quote.body}"</p>
                             <p className="font-light"><Link to={`/authors/${data.quote.authorId}`}>{data.quote.authorName}</Link></p>
                         </div>
-                    </Link>
+                    </Link> */}
                     <QuoteNoteDeleteCard quoteNote={data}/>
-                    {/* <div className="flex flex-col gap-1 bg-stone-800 p-4 rounded-lg max-w-2xl">
-                        <div className="flex flex-col py-3 border-b border-stone-700 w-full">
-                            <p className="text-sm font-semibold tracking-wider uppercase">Note ID: </p>
-                            <p className="truncate ..."><span className="font-thin text-lg">{data.data.id}</span></p>
-                        </div>
-                        <div className="flex flex-col py-3 border-b border-stone-700 w-full last:border-0">
-                            <p className="text-sm font-semibold tracking-wider uppercase">Note created: </p>
-                            <p><span className="font-thin text-lg">{data.data.createdAt}</span></p>
-                        </div>
-                        <div className="mt-6 flex flex-col">
-                            <Form method="post">
-                                <button
-                                    name="_method"
-                                    value="delete"
-                                    className="px-4 py-2 bg-blue-400 rounded text-white hover:bg-blue-600 flex w-full justify-center"
-                                >
-                                    Delete Note
-                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                    </svg>
-                                </button>
-                            </Form>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </div>
