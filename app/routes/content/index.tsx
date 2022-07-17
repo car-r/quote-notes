558c693967e2061933039fe57197a9a98b0b4aa5
Carson Richter <carson.ri@protonmail.com>
2022-07-17 10:20:47 -0700

    ContentCard component moved to components folder

--- app/routes/content/index.tsx
+++ app/routes/content/index.tsx
@@ -1,5 +1,6 @@
 import { Link, useLoaderData } from "@remix-run/react"
 import AddContentCard from "~/components/AddContentCard"
+import ContentCard from "~/components/ContentCard"
 import PageTitle from "~/components/PageTitle"
 import { prisma } from "~/db.server"
 
@@ -14,11 +15,6 @@ export default function ContentIndex() {
     return (
         <div className="flex flex-col pt-10 max-w-4xl">
             <PageTitle children={`Your Content`}/>
-            {/* <div className="pb-6">
-                <h3 className="text-2xl tracking-wide font-semibold pb-2 border-stone-800 border-b-2">
-                Your Content
-                </h3>
-            </div> */}
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                 <Link to={`/content/new`}
                     className="p-4 border border-stone-800 bg-stone-700 outline-dashed rounded-md text-stone-300/60"
@@ -27,22 +23,8 @@ export default function ContentIndex() {
                 </Link>
                 
                 {data.map((content: any) => (
-                    <Link to={`/content/${content.id}`} key={content.id}
-                    className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
-                    >
-                        <div className="pb-2">
-                            <img src={content.imgUrl} alt={content.title} 
-                                className="object-fit max-w-96"
-                            />
-                        </div>
-                        <div>
-                            <p className="font-bold">
-                                {content.title}
-                            </p>     
-                            <p className="text-sm font-thin tracking-wider">
-                                {content.authorName}
-                            </p>               
-                        </div>
+                    <Link to={`/content/${content.id}`} key={content.id}>
+                        <ContentCard content={content}/>
                     </Link>
                 ))}
             </div>
