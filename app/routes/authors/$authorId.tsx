5fc5c8c25032d6584ddb89bc9b8c905dcbf25e81
Carson Richter <carson.ri@protonmail.com>
2022-07-17 10:49:27 -0700

    moved QuoteIndexCard into components folder and put ContentCard component into authorId route

--- app/routes/authors/$authorId.tsx
+++ app/routes/authors/$authorId.tsx
@@ -2,6 +2,7 @@ import { useLoaderData, Link, useParams } from "@remix-run/react"
 import { redirect } from "@remix-run/server-runtime"
 import { useState } from "react"
 import AuthorRouteAuthorCard from "~/components/AuthorRouteAuthorCard"
+import ContentCard from "~/components/ContentCard"
 import PageTitle from "~/components/PageTitle"
 import { prisma } from "~/db.server"
 
@@ -49,11 +50,6 @@ export default function AuthorDetail() {
     return (
         <div className="flex flex-col pt-10 max-w-4xl">
             <PageTitle children={data.author.name}/>
-            {/* <div className="pb-6">
-                <h3 className="text-2xl tracking-wide font-semibold pb-2 border-stone-800 border-b-2">
-                    {data.author.name}
-                </h3>
-            </div> */}
             <AuthorRouteAuthorCard author={data} />
             <div className="mb-8">
                 <div className="py-6">
@@ -64,9 +60,9 @@ export default function AuthorDetail() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 ">
                 {data.content.map((content: any) => (
                     <Link to={`/content/${content.id}`} key={content.id}
-                    className="p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
+                    
                     >
-                        <div className="pb-2">
+                        {/* <div className="pb-2">
                             <img src={content.imgUrl} alt={content.title} 
                                 className="object-fit max-w-96"
                             />
@@ -78,7 +74,8 @@ export default function AuthorDetail() {
                             <p className="text-sm font-thin tracking-wider">
                                 {content.authorName}
                             </p>               
-                        </div>
+                        </div> */}
+                        <ContentCard content={content}/>
                         </Link>
                     ))}
                 </div>
--- app/routes/quotes/index.tsx
+++ app/routes/quotes/index.tsx
@@ -3,6 +3,7 @@ import { json, redirect } from "@remix-run/server-runtime";
 import { useState } from "react";
 import AddQuoteCard from "~/components/AddQuoteCard";
 import PageTitle from "~/components/PageTitle";
+import QuoteIndexCard from "~/components/QuoteIndexCard";
 
 import { prisma } from "~/db.server";
 import { requireUserId } from "~/session.server";
@@ -26,8 +27,6 @@ export const action = async ({request}: any) => {
     const form = await request.formData()
     const id = form.get('id')
     const isFavorited = form.get('isFavorited')
-    // const stringIsFavorited = form.get('isFavorited')
-    // const isFavorited = JSON.parse(stringIsFavorited)
     console.log(id + isFavorited)
 
     const quote = await prisma.quote.update({
@@ -48,45 +47,13 @@ export default function QuotesIndex() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     <AddQuoteCard />
                     {data.quotes.map((quote: any) => (
-                        <div key={quote.id}
-                            className="flex flex-col p-4 border border-stone-800 bg-stone-800 rounded-md text-stone-300/60 hover:border-blue-400"
-                        >   
-                            <div className="flex-1">
-                                <Form method="post">
-                                    <div onClick={() => console.log('clicked')} className="flex justify-end mb-1">   
-                                        <div className="flex flex-col">
-                                        <input type="hidden" name="id" value={quote.id}/>
-                                        {/* <input type="hidden" name="isFavorited" value={!quote.isFavorited}/> */}
-                                        {quote.isFavorited === "isFavorited" ? <input type="hidden" name="isFavorited" value="notFavorited"/> : <input type="hidden" name="isFavorited" value="isFavorited"/>}
-                                        {/* <input type="checkbox" name="isFavorited" /> */}
-                                            <button type="submit" className="">
-                                                {quote.isFavorited === "isFavorited" ? 
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 20 20" fill="currentColor">
-                                                        <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                                                    </svg>
-                                                    :
-                                                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-right" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
-                                                    </svg>
-                                                }
-                                            </button>
-                                        </div>
-                                    </div>
-                                </Form>
-                                <Link to={`/quotes/${quote.id}`}>
-                                    <div className="flex flex-col h-5/6 justify-center">
-                                        <p className="text-xl text-center pb-6 italic font-semibold">"{quote.body}"</p>
-                                        <p className="font-light mt-auto"><Link to={`/authors/${quote.authorId}`}>{quote.authorName}</Link></p>
-                                    </div>
-                                </Link>
-                            </div>
-                        </div>))
+                        <div key={quote.id}>
+                            <QuoteIndexCard quote={quote} />
+                        </div>
+                        ))
                     }
                 </div>
             </div>
-            
-           
         </>
-        
     )
 }
