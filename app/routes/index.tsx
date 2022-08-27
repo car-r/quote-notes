ec5ac1d148c37deb7b9e5d61c26803eb04d30c2b
Carson Richter <carson.ri@protonmail.com>
2022-08-27 16:33:32 -0700

    updated styling to match add author, quote, and content cards

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -162,7 +162,7 @@ export default function Index() {
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
             <Link 
               to={`/content/new`}
-              className="p-4 border border-stone-800 bg-stone-700 outline-dashed rounded-md text-stone-300/60 hover:outline-blue-400">
+              className="">
               <AddContentCard />
             </Link>
           </div>
