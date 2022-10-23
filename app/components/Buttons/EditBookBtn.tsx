6ff03f9700e9387fb670103e3a03037bb1fbeac4
Carson Richter <carson.ri@protonmail.com>
2022-10-23 00:05:50 -0700

    useRef and useTransitions on buttons and state changes for bookId edit route

---
+++ app/components/Buttons/EditBookBtn.tsx
@@ -0,0 +1,14 @@
+import { Link } from "@remix-run/react";
+
+export default function EditBookBtn({data, edit, setEdit}: any) {
+    console.log('edit Book btn -->', data, edit)
+    return (
+        <>
+            <Link to={`/books/${data.book.id}/edit`} onClick={() => setEdit(!edit)}>
+                <div className="text-sm md:text-base px-3 py-1 border-2 border-blue-400 bg-blue-400 hover:bg-blue-500 hover:border-blue-500 text-white rounded text-center cursor-pointer">
+                    Edit Book
+                </div>
+            </Link>
+        </>
+    )
+}
---
+++ app/routes/books/$bookId/edit.tsx
@@ -0,0 +1,183 @@
+import { useActionData, useLoaderData, useOutletContext, useTransition } from "@remix-run/react"
+import { prisma } from "~/db.server"
+import { requireUserId } from "~/session.server";
+import { redirect } from "@remix-run/node";
+import { Form } from "@remix-run/react";
+import { useEffect, useRef } from "react";
+
+export const loader = async ({params, request}: any) => {
+    const userId = await requireUserId(request);
+    const data = await prisma.user.findUnique({
+        where: { id: userId },
+        include: {
+            book: {
+                where: { id: params.bookId},
+                include: {
+                    author: true,
+                }
+            },
+            authors: {
+                where: { userId: userId}
+            }
+          }
+    })
+    return {data}
+}
+
+export const action = async ({request}: any) => {
+    const userId = await requireUserId(request);
+    const form = await request.formData()
+    const authorId = form.get('authorId')
+    const selectAuthorId = form.get('selectAuthorId')
+    const body = form.get('body')
+    const bookId = form.get('bookId')
+    const authorName = form.get('authorName')
+    const id = form.get('id')
+    const isFavorited = form.get('isFavorited')
+    const title = form.get('title')
+    const imgUrl = form.get('imgUrl')
+    const selectAuthorName = form.get('selectAuthorName')
+    console.log(Object.fromEntries(form))
+
+    // Action to update Book
+    if (form.get('_method') === 'update') {
+        const authorId = selectAuthorId
+        const authorName = selectAuthorName
+
+        const errors = {
+            title: '',
+            imgUrl: ''
+        }
+
+        function checkTitleName(title: any) {
+            if(!title || title.length < 3) {
+                return errors.title = `Title too short`
+            }
+        }
+
+        checkTitleName(title)
+
+
+        const isValidImageUrl = new RegExp('(jpe?g|png|gif|bmp)$')
+
+        const validateImageUrl = (value: string) => {
+            if (!isValidImageUrl.test(value)) {
+                return errors.imgUrl = `Not a valid Image URL`
+            }
+        
+        }
+
+        validateImageUrl(imgUrl)
+
+        if (errors.title || errors.imgUrl) {
+            const values = Object.fromEntries(form)
+            return { errors, values }
+        }
+
+        await prisma.book.update({
+            where: { id: bookId },
+            data: { title: title, authorId: authorId, imgUrl: imgUrl, authorName: authorName }
+        })
+
+        await prisma.quote.updateMany({ 
+            where: { bookId: bookId },
+            data: { authorId: authorId, authorName: authorName }
+        })
+
+        return redirect(`/books/${bookId}`)
+
+    }
+
+    // Action to delete Book
+    if(form.get('_method') === 'deleteBook') {
+        await prisma.book.delete({where: {id: bookId}})
+        return redirect(`/books`)
+    }
+
+}
+
+
+
+export default function EditBook() {
+    const data = useLoaderData()
+    const actionData = useActionData()
+    let transition = useTransition()
+    let isUpdating = 
+        transition.state === "submitting" &&
+        transition.submission.formData.get("_method") === "update"
+
+    // let formRef = useRef()
+    const formRef = useRef<HTMLFormElement>(null)
+    const [edit, setEdit]: any = useOutletContext()
+
+    useEffect(() => {
+        if (!isUpdating) {
+            formRef.current?.reset();
+            // setEdit(false)
+        }
+        else if (isUpdating) {
+            setEdit(false)
+        }
+    },[isUpdating, setEdit])
+    
+
+    console.log('bookId Edit data --> ', data)
+    return (
+        <div className="p-4 border border-stone-800 bg-stone-800 rounded-md">
+            <div className="flex flex-col gap-4 md:w-80">
+            <Form method="post" ref={formRef}>
+                <div className="flex flex-col">
+                    <button type="submit" name="_method" value="deleteBook" className="flex justify-end relative hover:text-stone-100">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        </svg>
+                    </button>
+                    <div className="flex flex-col gap-6">
+                        <div className="flex flex-col gap-1">
+                            <label className="text-sm font-semibold tracking-wider uppercase">
+                                Title
+                            </label>
+                            <input type="text" name="title" className="px-2 border border-stone-800 bg-stone-700 rounded" defaultValue={data.data.book[0].title}/>
+                            {actionData?.errors.title && (
+                                <p className="text-red-400 text-sm">{actionData.errors.title}</p>
+                            )}
+                        </div>
+
+                        <div className="flex flex-col gap-1">
+                            <label className="text-sm font-semibold tracking-wider uppercase">
+                                Author
+                            </label>
+                            <select name="selectAuthorId" className="bg-stone-700 rounded-sm p-1" >
+                                <option value={data.data.book[0].authorId}>{data.data.book[0].author.name}</option>
+
+                                {data.data.authors.filter((author: any) => author.id !== data.data.book[0].authorId).map((author: any) => (
+                                    <option key={author.id}  value={author.id}>{author.name}</option>
+                            ))}
+                            </select>
+                        </div>
+                
+                        <div className="flex flex-col gap-1">
+                            <label className="text-sm font-semibold tracking-wider uppercase">
+                                Image URL
+                            </label>
+                            <input type="text" name="imgUrl" className="px-2 border border-stone-800 bg-stone-700 rounded" defaultValue={data.data.book[0].imgUrl}/>
+                            {actionData?.errors.imgUrl && (
+                                <p className="text-red-400 text-sm">{actionData.errors.imgUrl}</p>
+                            )}
+                        </div>
+
+                        <div className="hidden">
+                            <input type="hidden" name="bookId" value={data.data.book[0].id}/>
+                        </div>
+                    </div>
+                    <div className="flex flex-col mt-6">
+                        <button type="submit" name="_method" value="update"  className="px-6 py-2 bg-blue-400 hover:bg-blue-600 text-white rounded">
+                            {isUpdating ? "Updating..." : "Update Book"}
+                        </button> 
+                    </div>
+                </div>
+            </Form>
+            </div>
+        </div>
+    )
+}
