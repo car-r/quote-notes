76e5d422605adece605172d6ea71d744f18bc97f
Carson Richter <carson.ri@protonmail.com>
2022-10-23 14:20:33 -0700

    loader refactor on quoteId and 404 error handling

--- app/components/QuoteNoteGrid.tsx
+++ app/components/QuoteNoteGrid.tsx
@@ -2,9 +2,10 @@ import { Link } from "@remix-run/react";
 import QuoteNote from "./QuoteNote";
 
 export default function QuoteNoteGrid({quote}: any) {
+    console.log('quote note grid --> ', quote)
     return (
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-            {quote.notes.map((note: any) => (
+            {quote.quote.note.map((note: any) => (
                 <Link to={`/quotenotes/${note.id}`} key={note.id} className="flex" >
                     <QuoteNote note={note}/>
                 </Link>
