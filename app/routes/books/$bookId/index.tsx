553766b7910a6a12e8a3fd1a1b9de8b9211703d6
Carson Richter <carson.ri@protonmail.com>
2022-10-29 23:37:05 -0700

    updated loader on bookId route for bookIdcard and testing fetch from openlibrary

--- app/routes/books/$bookId/index.tsx
+++ app/routes/books/$bookId/index.tsx
@@ -3,27 +3,49 @@ import { requireUserId } from "~/session.server";
 import { useLoaderData } from "@remix-run/react";
 import BookIdCard from "~/components/BookIdCard";
 
+
+
 export const loader = async ({params, request}: any) => {
-    const userId = await requireUserId(request);
+    // const userId = await requireUserId(request);
 
-    const data = await prisma.user.findUnique({
-        where: { id: userId },
+    const data = await prisma.book.findUnique({
+        where: {id: params.bookId},
         include: {
-            _count: {
-              select: {
-
-                book: true,
-                quoteNote: true
-              }
-            },
-            book: {
-                where: { id: params.bookId},
-                include: {
-                    author: true,
-                }
-            }
-          }
+            author: true
+        }
     })
+
+    if (!data) {
+        throw new Response("Can't find book.", {
+            status: 404,
+        })
+    }
+
+    // const response = await fetch(`https://openlibrary.org/isbn/${data.ISBN}.json`)
+    // const res = response.json()
+        // .then((response) => response.json())
+        // .then((openLibrary) => console.log(openLibrary))
+
+    
+
+    // const data = await prisma.user.findUnique({
+    //     where: { id: userId },
+    //     include: {
+    //         _count: {
+    //           select: {
+
+    //             book: true,
+    //             quoteNote: true
+    //           }
+    //         },
+    //         book: {
+    //             where: { id: params.bookId},
+    //             include: {
+    //                 author: true,
+    //             }
+    //         }
+    //       }
+    // })
 
     return {data}
 }
