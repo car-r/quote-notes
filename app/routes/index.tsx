bceeffda5f6912f060a0a737a0898f1a942910ec
Carson Richter <carson.ri@protonmail.com>
2022-10-31 15:25:19 -0700

    comment out author card on index route for merge fix

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Link, useLoaderData, } from "@remix-run/react";
-import AuthorCard from "~/components/AuthorCard";
+// import AuthorCard from "~/components/AuthorCard";
 import SectionTitle from "~/components/SectionTitle";
 import { prisma } from "~/db.server";
 import { requireUserId } from "~/session.server";
