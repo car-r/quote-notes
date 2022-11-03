eeaec48c152ef5e7ec8d935862b9882d7a7cdce2
Carson Richter <carson.ri@protonmail.com>
2022-11-03 10:52:26 -0700

    remove edit button from bookidcard

--- app/components/BookIdCard.tsx
+++ app/components/BookIdCard.tsx
@@ -23,19 +23,17 @@ export default function BookIdCard({data}: any) {
                     className="object-fit md:max-w-xs relative" />
             </div>
              <div>
-                <div className="flex justify-between ">
+                <div className="flex ">
                     <p className="font-bold w-10/12">
                         {data.data.title}
                     </p>
-                    <div className="flex text-xs justify-end" onClick={() => setEdit(!edit)}>
-
+                    {/* <div className="flex text-xs justify-end" onClick={() => setEdit(!edit)}>
                         <Link to={`/books/${data.data.id}/edit`}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 hover:text-white mr-1">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
                             </svg>
                         </Link>
-
-                    </div>
+                    </div> */}
                 </div>
                 <div className="flex">
                     <Link to={`/authors/${data.data.authorId}`}>
