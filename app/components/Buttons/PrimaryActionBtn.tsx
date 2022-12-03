e5cad9aa12a8fe8f2bd65e8501cdb2a3f774ccf8
Carson Richter <carson.ri@protonmail.com>
2022-12-02 21:02:58 -0700

    updates to hero section in landingpage

--- app/components/Buttons/PrimaryActionBtn.tsx
+++ app/components/Buttons/PrimaryActionBtn.tsx
@@ -1,10 +1,10 @@
 export default function PrimaryActionBtn({children}: any) {
     return (
-        <button 
-            className="px-4 py-2 font-semibold text-white  bg-blue-400 transition-all hover:ease-in-out 
+        <div 
+            className=" px-4 py-2 font-semibold text-white  bg-blue-400 transition-all hover:ease-in-out 
              hover:bg-blue-600 rounded text-center cursor-pointer"
         >
             {children}
-        </button>
+        </div>
     )
 }
