263ac1c8bb2b7e26e96937dd0ddead0b6719db22
Carson Richter <carson.ri@protonmail.com>
2022-12-04 10:43:37 -0700

    styling changes, hero images, blog post

--- app/components/LandingPage/HeroSection.tsx
+++ app/components/LandingPage/HeroSection.tsx
@@ -12,17 +12,17 @@ export default function HeroSection() {
                         </p>
                         <div className="flex gap-4">
                             <Link to="/join">
-                                <button className="flex tems-center gap-2 font-bold text-stone-800 border-2 border-blue-400 bg-blue-400 rounded-3xl px-4 py-2 hover:bg-transparent hover:text-white transition-all ease-in-out">
+                                <button className="flex items-center gap-2 font-bold text-sm md:text-base text-stone-800 border-2 border-blue-400 bg-blue-400 rounded-3xl px-4 py-2 hover:bg-transparent hover:text-white transition-all ease-in-out">
                                     <p>Get Started</p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="hidden sm:inline-block w-5 h-5 md:w-6 md:h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                                     </svg>
                                 </button>
                             </Link>
                             <Link to="/join">
-                                <button className="flex tems-center gap-2 font-bold text-stone-200 border-2 border-stone-200 bg-stone-900 rounded-3xl px-4 py-2 hover:bg-stone-50  hover:text-blue-600 transition-all ease-in-out">
+                                <button className="flex items-center gap-2 font-bold text-sm md:text-base text-stone-200 border-2 border-stone-200 bg-stone-900 rounded-3xl px-4 py-2 hover:bg-stone-50  hover:text-blue-600 transition-all ease-in-out">
                                     <p>Watch a Demo</p>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="hidden sm:inline-block w-5 h-5 md:w-6 md:h-6">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M6 20.25h12m-7.5-3v3m3-3v3m-10.125-3h17.25c.621 0 1.125-.504 1.125-1.125V4.875c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125z" />
                                     </svg>
                                 </button>
@@ -32,10 +32,16 @@ export default function HeroSection() {
                     <div className="flex flex-col mx-auto items-center">
                         <img
                             className="hidden lg:inline-block p-3 bg-stone-700/70 border border-stone-700 max-w-md rounded-lg skew-y-12 rotate-2" 
-                            src="herosection-image-2.jpg" alt="display of the quotenotes application"
+                            src="herosection-image.jpg" alt="display of the quotenotes application"
                         />
                         <div className="hidden lg:inline-block bg-stone-700/70 mt-1 w-32 h-10 skew-y-12 rotate-2 rounded"></div>
                         <div className="hidden lg:inline-block bg-stone-700/70 mt-1 w-48 h-2 skew-y-12 rotate-2 rounded"></div>
+                    </div>
+                    <div className="flex flex-col mx-auto items-center mt-20">
+                        <img
+                            className="lg:hidden p-3 rounded-2xl bg-stone-700/70 border border-stone-700 max-w-md skew-y-12 rotate-2 max-h-96 md:max-h-[32rem]" 
+                            src="herosection-mobile-image.jpg" alt="display of the quotenotes application"
+                        />
                     </div>
                 </div>
             </div>
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,6 +10,7 @@ import * as React from "react";
 import { createUserSession, getUserId } from "~/session.server";
 import { verifyLogin } from "~/models/user.server";
 import { safeRedirect, validateEmail } from "~/utils";
+import NavBar from "~/components/LandingPage/NavBar";
 
 export const loader: LoaderFunction = async ({ request }) => {
   const userId = await getUserId(request);
@@ -91,100 +92,103 @@ export default function LoginPage() {
   }, [actionData]);
 
   return (
-    <div className="flex min-h-screen flex-col justify-center ">
-      <div className="mx-auto w-full max-w-md px-8">
-        <Form method="post" className="space-y-6">
-          <div>
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium text-stone-400"
-            >
-              Email address
-            </label>
-            <div className="mt-1">
-              <input
-                ref={emailRef}
-                id="email"
-                required
-                autoFocus={true}
-                name="email"
-                type="email"
-                autoComplete="email"
-                aria-invalid={actionData?.errors?.email ? true : undefined}
-                aria-describedby="email-error"
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              />
-              {actionData?.errors?.email && (
-                <div className="pt-1 text-red-700" id="email-error">
-                  {actionData.errors.email}
-                </div>
-              )}
-            </div>
-          </div>
-
-          <div>
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium text-stone-400"
-            >
-              Password
-            </label>
-            <div className="mt-1">
-              <input
-                id="password"
-                ref={passwordRef}
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                aria-invalid={actionData?.errors?.password ? true : undefined}
-                aria-describedby="password-error"
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-              />
-              {actionData?.errors?.password && (
-                <div className="pt-1 text-red-700" id="password-error">
-                  {actionData.errors.password}
-                </div>
-              )}
-            </div>
-          </div>
-
-          <input type="hidden" name="redirectTo" value={redirectTo} />
-          <button
-            type="submit"
-            className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
-          >
-            Log in
-          </button>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                id="remember"
-                name="remember"
-                type="checkbox"
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-              />
+    <>
+      <NavBar />
+      <div className="flex min-h-screen flex-col justify-center ">
+        <div className="mx-auto w-full max-w-md px-8">
+          <Form method="post" className="space-y-6  -mt-40">
+            <div>
               <label
-                htmlFor="remember"
-                className="ml-2 block text-sm text-stone-500"
+                htmlFor="email"
+                className="block text-sm font-medium text-stone-400"
               >
-                Remember me
+                Email address
               </label>
+              <div className="mt-1">
+                <input
+                  ref={emailRef}
+                  id="email"
+                  required
+                  autoFocus={true}
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  aria-invalid={actionData?.errors?.email ? true : undefined}
+                  aria-describedby="email-error"
+                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+                />
+                {actionData?.errors?.email && (
+                  <div className="pt-1 text-red-700" id="email-error">
+                    {actionData.errors.email}
+                  </div>
+                )}
+              </div>
             </div>
-            <div className="text-center text-sm text-stone-500">
-              Don't have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{
-                  pathname: "/join",
-                  search: searchParams.toString(),
-                }}
+
+            <div>
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium text-stone-400"
               >
-                Sign up
-              </Link>
+                Password
+              </label>
+              <div className="mt-1">
+                <input
+                  id="password"
+                  ref={passwordRef}
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  aria-invalid={actionData?.errors?.password ? true : undefined}
+                  aria-describedby="password-error"
+                  className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
+                />
+                {actionData?.errors?.password && (
+                  <div className="pt-1 text-red-700" id="password-error">
+                    {actionData.errors.password}
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
-        </Form>
+
+            <input type="hidden" name="redirectTo" value={redirectTo} />
+            <button
+              type="submit"
+              className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
+            >
+              Log in
+            </button>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember"
+                  name="remember"
+                  type="checkbox"
+                  className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+                />
+                <label
+                  htmlFor="remember"
+                  className="ml-2 block text-sm text-stone-500"
+                >
+                  Remember me
+                </label>
+              </div>
+              <div className="text-center text-sm text-stone-500">
+                Don't have an account?{" "}
+                <Link
+                  className="text-blue-500 underline"
+                  to={{
+                    pathname: "/join",
+                    search: searchParams.toString(),
+                  }}
+                >
+                  Sign up
+                </Link>
+              </div>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
