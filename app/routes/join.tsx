8f6d770ea63d6c375f550b7038e9b83aa7ba258e
Carson Richter <carson.ri@protonmail.com>
2022-12-09 22:05:35 -0700

    return join.tsx to previous state

--- app/routes/join.tsx
+++ app/routes/join.tsx
@@ -30,8 +30,8 @@ export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
-  // const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
-  const redirectTo = formData.get("redirectTo");
+  const redirectTo = safeRedirect(formData.get("redirectTo"), "/");
+  // const redirectTo = formData.get("redirectTo");
 
   if (!validateEmail(email)) {
     return json<ActionData>(
